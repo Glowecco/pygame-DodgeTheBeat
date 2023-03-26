18e6befcc57b6b9c7642d8494167c080b4ceb49e
Ethan Oh <glow@MacBook-Pro.local>
2023-03-26 16:56:09 +0900

    created subfolders to place the graphics and fonts

--- act.py
+++ act.py
@@ -5,16 +5,20 @@ pygame.init()
 screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('yoyo')
 clock = pygame.time.Clock()
-font = pygame.font.Font('OpenSans-Regular.ttf', 50)
+font = pygame.font.Font('font/OpenSans-Regular.ttf', 50)
 
-background = pygame.image.load('sky.png')
+#surface 1
+background = pygame.image.load('bgi/sky.png')
 background = pygame.transform.scale(background, (810, 510))
 
-platform = pygame.image.load('ground.png')
+#surface 2
+platform = pygame.image.load('bgi/ground.png')
 platform = pygame.transform.scale(platform, (700, 100))
 
+#texts
 text = font.render('My Game', True, 'yellow')
 
+#main loop
 while True:
 
     for event in pygame.event.get():
@@ -24,7 +28,6 @@ while True:
         
     screen.blit(background, (0,0))
     screen.blit(platform, (50,400))
-    screen.blit(text, (320, 150))
 
     pygame.display.update()
     clock.tick(60)
