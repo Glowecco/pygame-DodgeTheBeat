a86fb40678baa564b54aa7fca97a11c9c27c1228
Ethan Oh <glow@MacBook-Pro.local>
2023-03-31 03:44:52 +0900

    optimized code adding Player class

--- act.py
+++ act.py
@@ -1,148 +1,59 @@
 import pygame
+from playerClass import Player
 from sys import exit
 
-def playerAnimation():
-    global playerShow, playerCostume_Index, playerCostume_Index_2, animationSpeed, keys
-
-    if playerRect.bottom == 400 and running==False: #stand
-        playerShow = playerCostume_StandJump[int(playerCostume_Index)]
-        playerCostume_Index += animationSpeed
-        if playerCostume_Index >= 7.7:
-            animationSpeed = - 0.3
-        elif playerCostume_Index < 0.3:
-            animationSpeed = 0.3
-    elif playerRect.bottom != 400: #jump
-        playerShow = playerJump
-    elif keys[pygame.K_LEFT]:
-        if playerCostume_Index_2 >= 9.7:
-            animationSpeed = -0.5
-        elif playerCostume_Index_2 < 0:
-            animationSpeed = 0.5
-        playerShow = playerCostume_Run[int(playerCostume_Index_2)]
-        playerCostume_Index_2 += animationSpeed
-        print(playerCostume_Index_2)
-        print('speed?: '+str(animationSpeed))   
-    elif keys[pygame.K_RIGHT]:
-        if playerCostume_Index_2 >= 9.7:
-            animationSpeed = -0.5
-        elif playerCostume_Index_2 < 0:
-            animationSpeed = 0.5
-        playerShow = playerCostume_Run[int(playerCostume_Index_2)]
-        playerCostume_Index_2 += animationSpeed
-        print(playerCostume_Index_2)
-        print('speed?: '+str(animationSpeed))
-
-
-
+#constants
 pygame.init()
-animationSpeed = 0.3
-flipLeft = False
-running = False
 screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('Dodge the Beat')
 clock = pygame.time.Clock()
 font = pygame.font.Font('font/OpenSans-Regular.ttf', 50)
 
-#surface 1
+#background surfaces
 background = pygame.image.load('bgi/sky.png').convert()
 background = pygame.transform.scale(background, (810, 510))
-
-#surface 2
 platform = pygame.image.load('bgi/ground.png').convert_alpha()
 platform = pygame.transform.scale(platform, (700, 100))
 
 #texts
 text = font.render('My Game', True, 'yellow')
 
-#player
-playerStand_1 = pygame.image.load('playerStand/rabbit -1.png').convert_alpha()
-playerStand_1 = pygame.transform.scale(playerStand_1, (27, 42))
-playerStand_2 = pygame.image.load('playerStand/rabbit -2.png').convert_alpha()
-playerStand_2 = pygame.transform.scale(playerStand_2, (27, 42))
-playerStand_3 = pygame.image.load('playerStand/rabbit -3.png').convert_alpha()
-playerStand_3 = pygame.transform.scale(playerStand_3, (27, 42))
-playerStand_4 = pygame.image.load('playerStand/rabbit -4.png').convert_alpha()
-playerStand_4 = pygame.transform.scale(playerStand_4, (27, 42))
-playerStand_5 = pygame.image.load('playerStand/rabbit -5.png').convert_alpha()
-playerStand_5 = pygame.transform.scale(playerStand_5, (27, 42))
-playerStand_6 = pygame.image.load('playerStand/rabbit -6.png').convert_alpha()
-playerStand_6 = pygame.transform.scale(playerStand_6, (27, 42))
-playerStand_7 = pygame.image.load('playerStand/rabbit -7.png').convert_alpha()
-playerStand_7 = pygame.transform.scale(playerStand_7, (27, 42))
-playerStand_8 = pygame.image.load('playerStand/rabbit -8.png').convert_alpha()
-playerStand_8 = pygame.transform.scale(playerStand_8, (27, 42))
-playerJump = pygame.image.load('playerJump/rabbit jump -2.png').convert_alpha()
-playerJump = pygame.transform.scale(playerJump, (28, 45))
-playerRun_1 = pygame.image.load('playerRun/rabbit run -1.png').convert_alpha()
-playerRun_1 = pygame.transform.scale(playerRun_1, (30, 43.5))
-playerRun_2 = pygame.image.load('playerRun/rabbit run -2.png').convert_alpha()
-playerRun_2 = pygame.transform.scale(playerRun_2, (30, 43.5))
-playerRun_3 = pygame.image.load('playerRun/rabbit run -3.png').convert_alpha()
-playerRun_3 = pygame.transform.scale(playerRun_3, (30, 43.5))
-playerRun_4 = pygame.image.load('playerRun/rabbit run -4.png').convert_alpha()
-playerRun_4 = pygame.transform.scale(playerRun_4, (30, 43.5))
-playerRun_5 = pygame.image.load('playerRun/rabbit run -5.png').convert_alpha()
-playerRun_5 = pygame.transform.scale(playerRun_5, (30, 43.5))
-playerRun_6 = pygame.image.load('playerRun/rabbit run -6.png').convert_alpha()
-playerRun_6 = pygame.transform.scale(playerRun_6, (30, 43.5))
-playerRun_7 = pygame.image.load('playerRun/rabbit run -7.png').convert_alpha()
-playerRun_7 = pygame.transform.scale(playerRun_7, (30, 43.5))
-playerRun_8 = pygame.image.load('playerRun/rabbit run -8.png').convert_alpha()
-playerRun_8 = pygame.transform.scale(playerRun_8, (30, 43.5))
-playerRun_9 = pygame.image.load('playerRun/rabbit run -9.png').convert_alpha()
-playerRun_9 = pygame.transform.scale(playerRun_9, (30, 43.5))
-playerRun_10 = pygame.image.load('playerRun/rabbit run -10.png').convert_alpha()
-playerRun_10 = pygame.transform.scale(playerRun_10, (30, 43.5))
+#download player images
+file_extensions = '.png'
+stand_images = []
+run_images = []
+for i in range(1, 9):
+    filename = f"{'playerStand/rabbit -'}{i}{file_extensions}"
+    image = pygame.image.load(filename).convert_alpha()
+    image = pygame.transform.scale(image, (27, 42))
+    stand_images.append(image)
+jump_image = pygame.image.load('playerJump/rabbit jump -2.png').convert_alpha()
+jump_image = pygame.transform.scale(jump_image, (28, 45))
+for i in range(1, 11):
+    filename = f"{'playerRun/rabbit run -'}{i}{file_extensions}"
+    image = pygame.image.load(filename).convert_alpha()
+    image = pygame.transform.scale(image, (30, 43.5))
+    run_images.append(image)
 
-playerCostume_Index = 0
-playerCostume_Index_2 = 0
-playerCostume_StandJump = [playerStand_1, playerStand_2, playerStand_3, playerStand_4
-    , playerStand_5, playerStand_6, playerStand_7, playerStand_8]
-playerCostume_Run = [playerRun_1, playerRun_2, playerRun_3, playerRun_4, 
-    playerRun_5, playerRun_6, playerRun_7, playerRun_8, playerRun_9, playerRun_10, ]
-
-playerShow = playerStand_1
-
-playerRect = playerShow.get_rect(midbottom = (400,400))
-gravity = 0
+#save class to >>player
+player = pygame.sprite.GroupSingle()
+player.add(Player(stand_images, jump_image, run_images))
 
 #main loop
+gravity = -1
 while True:
-    #check for quit and player jump
+    #check for quit 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN and playerRect.bottom >= 400:
-            if event.key == pygame.K_UP:
-                gravity = -7
-    
-    #player move left and right
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        playerRect.left -= 4
-        flipLeft = True
-        running = True
-    elif keys[pygame.K_RIGHT]:
-        playerRect.right += 4
-        flipLeft = False
-        running = True
-    else:
-        running = False
-        
-    #draw on screen
-    playerAnimation()
-    playerShow = pygame.transform.flip(playerShow, flipLeft, False)
+    #update and draw
     screen.blit(background, (0,0))
     screen.blit(platform, (50,400))
-    screen.blit(playerShow, playerRect)
-
-    #fall physics
-    gravity += 0.3
-    playerRect.y += gravity
-    if playerRect.bottom >= 400: 
-        playerRect.bottom = 400
-
-    #fps and update screen
+    player.update()
+    player.draw(screen)
     pygame.display.update()
+    #fps
     clock.tick(60)
+
+#for music: https://www.youtube.com/watch?v=Qz8hMjq2ARU
---
+++ playerClass.py
@@ -0,0 +1,86 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, stand, jump, run):
+        super().__init__()
+        #variable
+        self.playerCostume_Index = 0
+        self.playerCostume_Index_2 = 0
+        self.animationSpeed = 0.3
+        self.animationSpeed_2 = 0.3
+        self.flipLeft = False
+        self.running = False
+        self.gravity = -1
+        self.stand = stand
+        self.jump = jump
+        self.run = run
+
+        #setup
+        self.startImage = pygame.image.load('playerStand/rabbit -1.png').convert_alpha()
+        self.startImage = pygame.transform.scale(self.startImage, (27, 42))
+        self.image = self.startImage
+        self.rect = self.image.get_rect(midbottom = (500,350))
+        #save value
+        self.animation()
+    def animation(self):
+        #stand
+        keys = pygame.key.get_pressed()
+        if self.rect.bottom == 400 and self.running==False: 
+            self.image = self.stand[int(self.playerCostume_Index)]
+            self.playerCostume_Index += self.animationSpeed
+            if self.playerCostume_Index >= 7.5:
+                self.animationSpeed = - 0.3
+            elif self.playerCostume_Index < 0.5:
+                self.animationSpeed = 0.3
+        #jump
+        if self.rect.bottom != 400: 
+            self.image = self.jump
+        #left and right
+        elif keys[pygame.K_LEFT]:
+            if self.playerCostume_Index_2 >= 9.5:
+                self.animationSpeed_2 = -0.5
+            elif self.playerCostume_Index_2 < 0.5:
+                self.animationSpeed_2 = 0.5
+            self.image = self.run[int(self.playerCostume_Index_2)]
+            self.playerCostume_Index_2 += self.animationSpeed_2
+        elif keys[pygame.K_RIGHT]:
+            if self.playerCostume_Index_2 >= 9.5:
+                self.animationSpeed_2 = -0.5
+            elif self.playerCostume_Index_2 < 0.5:
+                self.animationSpeed_2 = 0.5
+            self.image = self.run[int(self.playerCostume_Index_2)]
+            self.playerCostume_Index_2 += self.animationSpeed_2
+        #flip image according to left/right
+        self.image = pygame.transform.flip(self.image, self.flipLeft, False)
+    def physicsLogic(self):
+        if self.rect.bottom == 400:
+            self.rect.y += self.gravity
+        else:
+            self.gravity += 0.3
+            self.rect.y += self.gravity
+
+        if 420 > self.rect.bottom >= 400 and 740>self.rect.x>33: 
+            self.rect.bottom = 400
+    
+        if self.rect.bottom >600:
+            exit() 
+    def movements(self):
+        keys = pygame.key.get_pressed()
+        #jump
+        if keys[pygame.K_UP] and 420 > self.rect.bottom >= 400 and 740>self.rect.x>33:
+            self.gravity = -7
+        #left and right
+        if keys[pygame.K_LEFT]:
+            self.rect.left -= 4
+            self.flipLeft = True
+            self.running = True
+        elif keys[pygame.K_RIGHT]:
+            self.rect.right += 4
+            self.flipLeft = False
+            self.running = True
+        else:
+            self.running = False        
+    def update(self):
+        self.physicsLogic()
+        self.movements()
+        self.animation()
